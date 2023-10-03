4bf671c46f626fe6589dc948ddd04643f032632e
ackatz <andrew@akatz.org>
2023-10-03 07:14:05 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="seclook",
-    version="0.1.3",
+    version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
