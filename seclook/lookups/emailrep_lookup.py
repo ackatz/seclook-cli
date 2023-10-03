295260640ca883a351cbb681d36cab38abcfdefe
ackatz <andrew@akatz.org>
2023-10-03 07:14:39 -0600

    Fix error if API not configured

--- seclook/lookups/emailrep_lookup.py
+++ seclook/lookups/emailrep_lookup.py
@@ -6,12 +6,11 @@ config_path = find_config()
 config = configparser.ConfigParser()
 config.read(config_path)
 
-emailrep_api_key = config["emailrep"]["api_key"]
-
 base_url = "https://emailrep.io/{}"
 
 
 def search(value):
+    emailrep_api_key = config["emailrep"]["api_key"]
     headers = {"Key": emailrep_api_key}
     response = requests.get(base_url.format(value), headers=headers)
     return response.json()
--- seclook/lookups/virustotal_lookup.py
+++ seclook/lookups/virustotal_lookup.py
@@ -6,12 +6,11 @@ config_path = find_config()
 config = configparser.ConfigParser()
 config.read(config_path)
 
-virustotal_api_key = config["virustotal"]["api_key"]
-
 base_url = "https://www.virustotal.com/api/v3/search"
 
 
 def search(value):
+    virustotal_api_key = config["virustotal"]["api_key"]
     headers = {"x-apikey": virustotal_api_key}
     params = {"query": value}
     response = requests.get(base_url, headers=headers, params=params)
