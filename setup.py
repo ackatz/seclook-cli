8b64102ce2f88376f9e85cd2ecd6423f0d4ecf2b
ackatz <andrew@akatz.org>
2023-10-08 13:25:30 -0600

    v0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author="Andrew Katz",
     author_email="andrew@akatz.org",
     license="MIT",
-    version="0.5.0",
+    version="0.5.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
